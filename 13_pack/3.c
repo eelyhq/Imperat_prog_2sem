b7524c513a705f0bf92ab620c55f7942cd95b606
denxl <denxl@vk.com>
2026-02-05 19:26:15 +0700

    fix

---
+++ 13_pack/3.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int  main() {
+    FILE* f_in = fopen("/home/eely/Imperat_prog_2sem/13_pack/input.txt","r");
+    FILE* f_out = fopen("/home/eely/Imperat_prog_2sem/13_pack/output.txt","w");
+
+    int c = ' ';
+    char s1 = '[';
+    char s2 = ']';
+    char s3 = '\n';
+    int f = 0;//флаг, идет ли сейчас строка  в двойных кавычках
+    int f2 = 0;//флаг, идет ли сейчас строка непрерывная без кавычек
+    int f3 = 0; //флаг, первая ли строка
+    while( (c = fgetc(f_in)) != '\n'){
+        if (c == '\"') {
+            if (f == 0) {
+                if (f3 == 1) {
+                    fprintf(f_out,"%c", s3);
+                }
+                f = 1;
+                fprintf(f_out,"%c", s1);
+                f3 = 1;
+            }
+            else if (f == 1) {
+                fprintf(f_out,"%c", s2);
+                f = 0;
+                f3 = 1;
+            }
+        }
+        else if (f == 1) {
+            fprintf(f_out,"%c", c);
+            f3 = 1;
+        }
+        else {
+
+            if (f2 == 0 && c != ' ') {
+                if (f3 == 1) {
+                    fprintf(f_out,"%c", s3);
+                }
+                fprintf(f_out,"%c", s1);
+                fprintf(f_out,"%c", c);
+                f3 = 1;
+                f2 = 1;
+            }
+            else if (f2 == 1 && c == ' ') {
+                fprintf(f_out,"%c", s2);
+                f3 = 1;
+                f2 = 0;
+            }
+            else if (f2 == 1) {
+                fprintf(f_out,"%c", c);
+                f3 = 1;
+            }
+        }
+    }
+    if (f2 == 1) {
+        fprintf(f_out,"%c", s2);
+        f3 = 1;
+    }
+    fclose(f_in);
+    fclose(f_out);
+    return 0;
+}
