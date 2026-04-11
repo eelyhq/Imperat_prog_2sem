061d315b090b850deb17900abfadf1caf09d3899
Denxl <denxl@vk.com>
2026-04-11 17:48:53 +0700

    fix

--- 17_pack/9.c
+++ 17_pack/9.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct vector{
     int* arr ;
@@ -36,34 +37,48 @@ int PUSH( vector* vec , int value ) {
     return vec->n - 1;
 }
 
-int T = 0;
-void dfs(vector** adj_list, int v, int* visited, int* t_out) {
+
+void dfs(vector** adj_list, int v, int* visited, int* t_out, int* num_t_out) {
     visited[v] = 1;
 
     for (int i = 0; i < adj_list[v] -> n; i++) {
         if (visited[adj_list[v] -> arr[i]] == 0) {
-            dfs(adj_list, adj_list[v] -> arr[i], visited, t_out );
+            dfs(adj_list, adj_list[v] -> arr[i], visited, t_out, num_t_out);
         }
     }
-    t_out[v] = T;
+    t_out[*num_t_out] = v;
+    *num_t_out += 1;
+}
 
-    T += 1;
+void dfs2(vector** adj_list, int v, int* visited, int* csc, int* curr_csc) {
+    visited[v] = 1;
+
+    for (int i = 0; i < adj_list[v] -> n; i++) {
+        if (visited[adj_list[v] -> arr[i]] == 0) {
+            dfs2(adj_list, adj_list[v] -> arr[i], visited, csc, curr_csc);
+        }
+    }
+
+    csc[v] = *curr_csc;
 }
 
 
 int main() {
-    FILE* f_in = fopen("/home/eely/CLionProjects/Imperat_prog_2sem/17_pack/input.txt", "r");
-    FILE* f_out = fopen("/home/eely/CLionProjects/Imperat_prog_2sem/17_pack/output.txt", "w");
+    FILE* f_in = fopen("input.txt", "r");
+    FILE* f_out = fopen("output.txt", "w");
 
-    int n, m; // n - ver, n - edges
+    int n, m; // n - ver, m - edges
 
-    fscanf(f_in, "%d %d", &m, &n);
+    fscanf(f_in, "%d %d", &n, &m);
 
     vector** adj_list = (vector**)malloc(sizeof(vector*)  * (n+1));
+    vector** adj_list_transpon = (vector**)malloc(sizeof(vector*)  * (n+1));
 
     for (int i = 1; i <= n; i++) {
-        adj_list[i] = (vector*)malloc(sizeof(int) * 1);
+        adj_list[i] = (vector*)malloc(sizeof(vector) * 1);
+        adj_list_transpon[i] = (vector*)malloc(sizeof(vector) * 1);
         INIT(adj_list[i]);
+        INIT(adj_list_transpon[i]);
     }
 
     for (int i = 0; i < m; i++) {
@@ -73,16 +88,54 @@ int main() {
             continue;
         }
         PUSH(adj_list[from], to);
+        PUSH(adj_list_transpon[to], from);
     }
 
-   int* visited = (int*)calloc(n+1,sizeof(int));
-
+    int* visited = (int*)calloc(n+1,sizeof(int));
     int* t_out = (int*)malloc((n+1) * sizeof(int));
 
-
+    int num_t_out = 0;
     for (int i = 1; i <= n; i++) {
         if (visited[i] == 0) {
-            dfs(adj_list, i, visited, t_out);
+            dfs(adj_list, i, visited, t_out, &num_t_out);
         }
     }
+
+    int* csc = (int*)malloc((n+1) * sizeof(int));
+    memset(visited, 0, (n+1) * sizeof(int));
+
+    int curr_csc = 1;
+    for (int i = num_t_out - 1; i >= 0 ; i--) // go for verices in order from max to min t_out
+    {
+        if (visited[t_out[i]] == 0)
+        {
+            dfs2(adj_list_transpon, t_out[i], visited, csc, &curr_csc);
+            curr_csc++;
+        }
+    }
+
+    fprintf(f_out, "%d\n", curr_csc-1);
+
+    for (int i = 1; i <= n; i++ )
+    {
+        fprintf(f_out, "%d ", csc[i]);
+    }
+
+    for (int i = 1; i <= n; i++) {
+        DESTROY(adj_list[i]);
+        DESTROY(adj_list_transpon[i]);
+        free(adj_list_transpon[i]);
+        free(adj_list[i]);
+    }
+
+    free(adj_list_transpon);
+    free(adj_list);
+    free(visited);
+    free(t_out);
+    free(csc);
+
+    fclose(f_in);
+    fclose(f_out);
+
+    return 0;
 }
